8edc589da0e96774a35385c809472fd53d357937
Matheus Carvalho <matheusfgc99@gmail.com>
2025-01-13 13:59:26 -0300

    0.0.0

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Doc by Matheus</v-toolbar-title>
+      <v-spacer />
+      <div class="d-flex align-center">
+        <v-switch
+          v-model="isDarkTheme"
+          @change="toggleTheme"
+          class="mt-6 mr-2"
+        />
+        <v-icon class="ml-2 mr-5">{{ isDarkTheme ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>Menu Item 1</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Menu Item 2</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Menu Item 3</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      isDarkTheme: localStorage.getItem('selectedTheme') === 'dark',
+    };
+  },
+  methods: {
+    toggleTheme() {
+      const newTheme = this.isDarkTheme ? 'dark' : 'light';
+      this.$vuetify.theme.global.name = newTheme; // Altera o tema global do Vuetify
+      localStorage.setItem('selectedTheme', newTheme); // Atualiza o tema no localStorage
+    }
+  },
+  mounted() {
+    // Define o tema inicialmente ao montar o componente
+    this.$vuetify.theme.global.name = this.isDarkTheme ? 'dark' : 'light';
+  }
+};
+</script>
+
+<style>
+/* Adicione seus estilos personalizados aqui */
+</style>
